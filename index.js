f8dbbcf9f09140a0eb25bda5e5f462ca09980e6d
Jeevan Johal <jkjohal@ucdavis.edu>
2023-03-20 18:34:53 -0700

    template corrections

--- index.js
+++ index.js
@@ -1,137 +1,136 @@
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
-const template = require('./src/page-template');
 const generatePage = require('./src/page-template');
 
 const questions  = [
     {
         type: 'input',
-        name: 'employeeName',
-        message: "What is the employee's name?"
+        name: 'employeeName1',
+        message: "What is the first employee's name?"
         
     },
     {
         type: 'input',
-        name: 'employeeID',
-        message: "What is the employee's ID number?"
+        name: 'employeeID1',
+        message: "What is the first employee's ID number?"
         
     },
     {
         type: 'input',
-        name: 'email',
-        message: "What is the employee's email address?"
+        name: 'email1',
+        message: "What is the first employee's email address?"
         
     },
     {
         type: 'list',
-        name: 'employeeType',
-        message: "What is the employee type?",
+        name: 'employeeType1',
+        message: "What is the first employee's type?",
         choices: ["manager", 'engineer', 'intern']
         
     },
     {
         type: 'input',
-        name: 'github',
-        message: "What is the employee's github username?"
+        name: 'github1',
+        message: "What is the first employee's github username?"
         
     },
     {
         type: 'input',
-        name: 'school',
-        message: "What is the employee's school?"
+        name: 'school1',
+        message: "What is the first employee's school?"
         
     },
     {
         type: 'input',
-        name: 'office',
-        message: "What is the employee's office number?"
+        name: 'office1',
+        message: "What is the first employee's office number?"
         
     },
     {
         type: 'input',
-        name: 'employeeName',
-        message: "What is the employee's name?"
+        name: 'employeeName2',
+        message: "What is the second employee's name?"
         
     },
     {
         type: 'input',
-        name: 'employeeID',
-        message: "What is the employee's ID number?"
+        name: 'employeeID2',
+        message: "What is the second employee's ID number?"
         
     },
     {
         type: 'input',
-        name: 'email',
-        message: "What is the employee's email address?"
+        name: 'email2',
+        message: "What is the second employee's email address?"
         
     },
     {
         type: 'list',
-        name: 'employeeType',
-        message: "What is the employee type?",
+        name: 'employeeType2',
+        message: "What is the second employee's type?",
         choices: ["manager", 'engineer', 'intern']
         
     },
     {
         type: 'input',
-        name: 'github',
-        message: "What is the employee's github username?"
+        name: 'github2',
+        message: "What is the second employee's github username?"
         
     },
     {
         type: 'input',
-        name: 'school',
-        message: "What is the employee's school?"
+        name: 'school2',
+        message: "What is the second employee's school?"
         
     },
     {
         type: 'input',
-        name: 'office',
-        message: "What is the employee's office number?"
+        name: 'office2',
+        message: "What is the second employee's office number?"
         
     },
     {
         type: 'input',
-        name: 'employeeName',
-        message: "What is the employee's name?"
+        name: 'employeeName3',
+        message: "What is the third employee's name?"
         
     },
     {
         type: 'input',
-        name: 'employeeID',
-        message: "What is the employee's ID number?"
+        name: 'employeeID3',
+        message: "What is the third employee's ID number?"
         
     },
     {
         type: 'input',
-        name: 'email',
-        message: "What is the employee's email address?"
+        name: 'email3',
+        message: "What is the third employee's email address?"
         
     },
     {
         type: 'list',
-        name: 'employeeType',
-        message: "What is the employee type?",
+        name: 'employeeType3',
+        message: "What is the third employee's type?",
         choices: ["manager", 'engineer', 'intern']
         
     },
     {
         type: 'input',
-        name: 'github',
-        message: "What is the employee's github username?"
+        name: 'github3',
+        message: "What is the third employee's github username?"
         
     },
     {
         type: 'input',
-        name: 'school',
-        message: "What is the employee's school?"
+        name: 'school3',
+        message: "What is the third employee's school?"
         
     },
     {
         type: 'input',
-        name: 'office',
-        message: "What is the employee's office number?"
+        name: 'office3',
+        message: "What is the third employee's office number?"
         
     },
 
--- src/page-template.js
+++ src/page-template.js
@@ -11,31 +11,31 @@ function generatePage(data){
 <body>
     <h1>Team Profile</h1>
     <div>
-        <h2>Employee name: ${data.employeeName}</h2>
-        <h3>Employee id: ${data.employeeID}</h3>
-        <h3>Employee email: ${data.email} </h3>
-        <h3>Employee type: ${data.employeeType}</h3>
-        <h3>Github: ${data.github}</h3>
-        <h3>School: ${data.school}</h3>
-        <h3>Office #: ${data.office}</h3>
+        <h2>Employee name: ${data.employeeName1}</h2>
+        <h3>Employee id: ${data.employeeID1}</h3>
+        <h3>Employee email: ${data.email1} </h3>
+        <h3>Employee type: ${data.employeeType1}</h3>
+        <h3>Github: ${data.github1}</h3>
+        <h3>School: ${data.school1}</h3>
+        <h3>Office #: ${data.office1}</h3>
     </div>
     <div>
-        <h2>Employee name: ${data.employeeName}</h2>
-        <h3>Employee id: ${data.employeeID}</h3>
-        <h3>Employee email: ${data.email} </h3>
-        <h3>Employee type: ${data.employeeType}</h3>
-        <h3>Github: ${data.github}</h3>
-        <h3>School: ${data.school}</h3>
-        <h3>Office #: ${data.office}</h3>
+        <h2>Employee name: ${data.employeeName2}</h2>
+        <h3>Employee id: ${data.employeeID2}</h3>
+        <h3>Employee email: ${data.email2} </h3>
+        <h3>Employee type: ${data.employeeType2}</h3>
+        <h3>Github: ${data.github2}</h3>
+        <h3>School: ${data.school2}</h3>
+        <h3>Office #: ${data.office2}</h3>
     </div>
     <div>
-        <h2>Employee name: ${data.employeeName}</h2>
-        <h3>Employee id: ${data.employeeID}</h3>
-        <h3>Employee email: ${data.email} </h3>
-        <h3>Employee type: ${data.employeeType}</h3>
-        <h3>Github: ${data.github}</h3>
-        <h3>School: ${data.school}</h3>
-        <h3>Office #: ${data.office}</h3>
+        <h2>Employee name: ${data.employeeName3}</h2>
+        <h3>Employee id: ${data.employeeID3}</h3>
+        <h3>Employee email: ${data.email3} </h3>
+        <h3>Employee type: ${data.employeeType3}</h3>
+        <h3>Github: ${data.github3}</h3>
+        <h3>School: ${data.school3}</h3>
+        <h3>Office #: ${data.office3}</h3>
     </div>
 </body>
 </html>
